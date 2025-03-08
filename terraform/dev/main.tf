7a720ba371da5d227cfcd35edf6bb58a1448db7b
Imran Raza Khan <imranrazakhan@gmail.com>
2025-03-08 02:47:36 +0100

    Added Terraform Shared-Infra 7

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -27,13 +27,6 @@ resource "google_container_cluster" "gke_autopilot_dev" {
     master_ipv4_cidr_block = "172.16.0.32/28"
   }
 
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = "95.90.235.167/32"  # ✅ Replace with your IP or office VPN
-      display_name = "Allowed Admin IP"
-    }
-  }
-
   network    = data.terraform_remote_state.shared_infra.outputs["vpc_id"]
   subnetwork = data.terraform_remote_state.shared_infra.outputs.subnet_ids[0]
 }
